08a9e857cb785590c6c24c922140501760e522f3
Anish <anish.singhani@gmail.com>
2019-07-20 11:27:35 -0700

    done

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
+var currentClass = ""
+
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add("animated", animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove("animated", animationName)
+        node.removeEventListener("animationend", handleAnimationEnd)
+    
+        if (typeof callback === "function") callback()
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd)
+}
+
+function getImage(objectClass) {
+    if(objectClass == "apple") {
+        return "apple_overlay.png"
+    } else if(objectClass == "banana") {
+        return "banana_overlay.png"
+    } else {
+        return ""
+    }
+}
+
 function classifyAndNext(model) {
-    console.log(pred)
 	model.detect(video).then(pred => {
         pred = pred.map(x => x.class)
         pred = pred.filter((value, index, arr) => arr.indexOf(value) == index)
 
-        console.log(pred)
+        var newClass = ""
 
         if(pred.includes("person")) {
-            document.getElementById("overlay").src = "apple_overlay.png"
+            newClass = "apple"
         } else if(pred.includes("banana")) {
-            document.getElementById("overlay").src = "banana_overlay.png"
+            newClass = "banana"
         } else {
-            document.getElementById("overlay").src = ""
+            newClass = ""
+        } 
+
+        if(currentClass != newClass) {
+            if(currentClass == "") {
+                // new card coming in
+                document.getElementById("overlay").src = getImage(newClass)
+                animateCSS("#overlay", "slideInUp", function() {
+
+                })
+            }
+            else if(newClass == "") {
+                // card sliding out
+                animateCSS("#overlay", "slideOutDown", function() {
+                    document.getElementById("overlay").src = ""
+                })
+            }
+            else {
+                // change from one card to another
+                document.getElementById("overlay").src = getImage(newClass)
+            }
+
+            currentClass = newClass
         }
+
+
 
 		setTimeout(function() {
 			classifyAndNext(model)
@@ -20,9 +69,10 @@ function classifyAndNext(model) {
 	});
 }
 
-var video = document.getElementById('video');
+var video = document.getElementById("video");
 
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
 	navigator.mediaDevices.getUserMedia({ video: { facingMode: { ideal: "environment" } } }).then(function(stream) {
 		video.srcObject = stream;
 		video.play();
